e08b76c5890d618cedc0c43c1a858def13f53868
MattChuang0 <bxiang0520@icloud.com>
2021-07-15 23:12:56 +0800

    2
    create album

--- ShootShot/Controllers/PhotAlbumController.cs
+++ ShootShot/Controllers/PhotAlbumController.cs
@@ -11,39 +11,12 @@ namespace ShootShot.Controllers
 {
     public class PhotAlbumController : Controller
     {
-        string constr = @"Data Source=.;Initial Catalog=dbShootShot;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
-        private List<tPhotAlbum> GetPhotAlbums()
-        {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = constr;
-            SqlDataAdapter adp = new SqlDataAdapter("select * from tPhotAlbum", con);
-            DataSet ds = new DataSet();
-            adp.Fill(ds);
-            DataTable dt = ds.Tables[0];
-            List<tPhotAlbum> photAlbums = new List<tPhotAlbum>();
-            for(int i = 0; i < dt.Rows.Count; i++)
-            {
-                photAlbums.Add(new tPhotAlbum
-                {
-                    fName = dt.Rows[i]["fName"].ToString()
-                });
-            }
-            return photAlbums;
-        }
-        
-        private void ExecutedCmd(SqlCommand cmd)
-        {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = constr;
-            con.Open();
-            cmd.Connection = con;
-            cmd.ExecuteNonQuery();
-            con.Close();
-        }
+        dbShootShotEntities db = new dbShootShotEntities();
         // GET: PhotAlbum
         public ActionResult Index()
         {
-            return View();
+            var albums = db.tPhotAlbum.ToList();
+            return View(albums);
         }
         public ActionResult Create()
         {
@@ -55,19 +28,15 @@ namespace ShootShot.Controllers
             if (ModelState.IsValid)
             {
                 ViewBag.Error = false;
-                try
-                {
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.CommandText = "insert into tPhotAlbum(fName)" + "values(@fName)";
-                    cmd.Parameters.Add(new SqlParameter("@fName", SqlDbType.NVarChar)).Value = album.fName;
-                    ExecutedCmd(cmd);
-                    return RedirectToAction("Index");
-                }
-                catch(Exception ex)
+                var temp = db.tPhotAlbum.Where(m => m.fEmail == album.fEmail).FirstOrDefault();
+                if (temp != null)
                 {
                     ViewBag.Error = true;
                     return View(album);
                 }
+                db.tPhotAlbum.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View(album);
         }
